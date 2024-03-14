50504c089d798b315ab342202534c72a6e6b8080
JohanDelao <johandelao10@gmail.com>
2024-03-14 16:37:55 -0400

    making navbar dynamic + persistent

--- topics-project/app/(landing-page)/page.js
+++ topics-project/app/(landing-page)/page.js
@@ -1,17 +1,16 @@
-import { Navbar } from "./_components/navbar";
 import { Hero } from "./_components/hero";
 import { Infographics } from "./_components/infographics";
 
 const LandingPage = () => {
     return (
-      <div className="min-h-screen flex flex-col bg-customPrimary w-full">
-          <div className="flex flex-col bg-customSecondary gap-5 md:min-h-[calc(100vh/2)]">
-                <Navbar />
+      // Subtracting 64px accounts for the navbar height (64px) in the layout to make page non-scrollable
+      <div className="min-h-[calc(100vh-64px)] flex flex-col bg-customPrimary w-full">
+          <div className="flex flex-col bg-customSecondary md:min-h-[calc(calc(100vh/2)-64px)]">
                 <div className="h-full my-auto">
                     <Hero />
                 </div>
           </div>
-          <div className="flex flex-col md:min-h-[calc(100vh/2)] justify-center items-center w-full">
+          <div className="flex flex-col md:min-h-[calc(calc(100vh/2)-64px)] justify-center items-center w-full">
             <Infographics />
           </div>
       </div>
