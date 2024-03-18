66c54417ed20f16e51abc2896dc71deb4888e4a2
Henry <hsuarez000@citymail.cuny.edu>
2024-03-18 18:01:08 -0400

    Step 1 - Unit tests for sendData function

---
+++ topics-project/__tests__/sendData.test.js
@@ -0,0 +1,107 @@
+// Import the module where sendData function is defined
+const { sendData } = require('../app/customfoods/action.js');
+
+// Mock the Supabase client
+jest.mock('../src/utils/supabase/server', () => ({
+  createClient: () => ({
+    from: () => ({
+      insert: jest.fn(() => {
+        return Promise.resolve({ data: { id: 1 }, error: null });
+      }),
+    }),
+  }),
+}));
+
+describe('sendData function', () => {
+  let errorSpy;
+
+  beforeEach(() => {
+    errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    errorSpy.mockRestore();
+  });
+
+
+  it('logs an error when food_name is empty', async () => {
+    const incorrectFormData = {
+      food_name: '',
+      calories: 1,
+      protein: 0,
+      fat: 0,
+      carbs: 25,
+    };
+
+    await sendData(incorrectFormData);
+
+    expect(errorSpy).toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalledWith("Error: Food name is required."); // checks for an error from an empty food name
+  });
+
+  it('logs an error when calories is a string', async () => {
+    const incorrectFormData = {
+      food_name: 'Test_Food',
+      calories: 'Not a number',
+      protein: 0,
+      fat: 0,
+      carbs: 25,
+    };
+
+    await sendData(incorrectFormData);
+
+    expect(errorSpy).toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalledWith("Error: Calories must be a positive number."); // expects for calories error
+  });
+
+  it('logs an error when protein is a string', async () => {
+    const incorrectFormData = {
+      food_name: 'Test_Food',
+      calories: 120,
+      protein: 'Not a number',
+      fat: 0,
+      carbs: 25,
+    };
+
+    await sendData(incorrectFormData);
+
+    expect(errorSpy).toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalledWith('Error: Protein must be a positive number.'); // expects for proiten error 
+  });
+  it('logs an error when fat is a string', async () => {
+    const incorrectFormData = {
+      food_name: 'Test_Food',
+      calories: 120,
+      protein: 20,
+      fat: 'Not a number',
+      carbs: 25,
+    };
+
+    await sendData(incorrectFormData);
+
+    expect(errorSpy).toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalledWith('Error: Fat must be a positive number.'); // expects for proiten error 
+  });
+
+  it('logs an error when carbs is a string', async () => {
+    const incorrectFormData = {
+      food_name: 'Test_Food',
+      calories: 120,
+      protein: 10,
+      fat: 0,
+      carbs: 'Not a number',
+    };
+
+    await sendData(incorrectFormData);
+
+    expect(errorSpy).toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalledWith('Error: Carbs must be a positive number.'); // expects for proiten error 
+  });
+
+  
+});
+  
