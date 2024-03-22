a1fe57cd854e929ef05eca77bdc7e3c97c5b6f18
JohanDelao <johandelao10@gmail.com>
2024-03-22 19:43:43 -0400

    add logic for github secrets for supabase

--- topics-project/__e2e__/signin-form.spec.js
+++ topics-project/__e2e__/signin-form.spec.js
@@ -1,6 +1,14 @@
 // @ts-check
 
 import { test, expect } from '@playwright/test';
+import { createClient } from '@supabase/supabase-js';
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  console.error('Supabase URL and Key are required!');
+  process.exit(1); // Exits the process if the variables are not found
+}
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 test('successfully sign into application', async ({ page }) => {
   await page.goto('http://localhost:3000');
