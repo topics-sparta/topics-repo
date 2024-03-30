a12e706627e09e1ac4938c7f1f6387064b67231c
Henry <hsuarez000@citymail.cuny.edu>
2024-03-29 20:51:30 -0400

    added some styling to sliders

--- topics-project/app/home/_components/macros.js
+++ topics-project/app/home/_components/macros.js
@@ -4,15 +4,15 @@ export const Macros = ({ protein, fat, carbs, proteinGoal, fatGoal, carbGoal }) 
     <div className="bg-customAccent/10 rounded-lg shadow-lg w-6/12 h-200 p-2 max-w-52 flex flex-col justify-between">
       <h1 className="text-base font-semibold font-poppins text-customAccent text-start md:text-lg">
         Macros
-        <span className="text-xs">(grams)</span>
+        <span className="text-xs pl-2">(grams)</span>
       </h1>
       <div className="relative flex flex-col w-full h-full">
         {/* The idea here is going to be we grab the users info and we will put min 0, max their targets, value current  */}
         {/*Protein Slider */}
         <div>
-          <h1>Protein: {protein}/{proteinGoal}</h1>
+          <h1 className="text-sm font-redHatText text-customAccent">Protein: {protein}/{proteinGoal}</h1>
           <input
-            className="appearance-none bg-customAccent rounded-full h-19"
+            className="appearance-none bg-customAccent rounded-full w-10/12"
             type="range"
             min="0"
             max={proteinGoal}
@@ -21,9 +21,9 @@ export const Macros = ({ protein, fat, carbs, proteinGoal, fatGoal, carbGoal }) 
         </div>
         {/*Fat Slider */}
         <div>
-        <h1>Fat: {fat}/{fatGoal}</h1>
+        <h1 className="text-sm font-redHatText text-customAccent">Fat: {fat}/{fatGoal}</h1>
           <input
-            className="appearance-none bg-customAccent rounded-full"
+            className="appearance-none bg-customAccent rounded-full w-10/12"
             type="range"
             min="0"
             max={fatGoal}
@@ -32,9 +32,9 @@ export const Macros = ({ protein, fat, carbs, proteinGoal, fatGoal, carbGoal }) 
         </div>
         {/*Carbs Slider */}
         <div>
-        <h1>Fat: {carbs}/{carbGoal}</h1>
+        <h1 className="text-sm font-redHatText text-customAccent">Carbs: {carbs}/{carbGoal}</h1>
           <input
-            className="appearance-none bg-customAccent rounded-full"
+            className="appearance-none bg-customAccent rounded-full w-10/12"
             type="range"
             min="0"
             max={carbGoal}
