ff360ff2e8684221d4de20a40daf4105570001bf
MansijMishra <mansijm822@gmail.com>
2024-03-30 09:23:50 -0400

    set up file structure for add food page and components

---
+++ topics-project/app/addfood/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function AddFoodPage() {
+
+
+  return (
+    <div className="w-full min-h-[calc(100vh-64px)] bg-customPrimary">
+      <div className="lg:max-w-screen-xl mx-auto px-4 xl:px-0 flex flex-col gap-8">
+    
+      </div>
+    </div>
+  );
+}
