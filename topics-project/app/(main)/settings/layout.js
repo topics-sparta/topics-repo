5adf6047d67932a97ff60a20347de99af0ec2502
JohanDelao <johandelao10@gmail.com>
2024-04-01 18:44:51 -0400

    fix mobile styling

--- topics-project/app/(main)/settings/layout.js
+++ topics-project/app/(main)/settings/layout.js
@@ -3,13 +3,13 @@ import NavbarDropDown from "./components/NavbarDropDown";
 
 export default function Layout({ children }) {
   return (
-    <div className="w-full h-full bg-customPrimary">
-        <div className="flex flex-col max-h-24 w-full items-center">
+    <div className="w-full h-full flex flex-col bg-customPrimary">
+        <div className="flex flex-col max-h-32 w-full items-center">
             <p className="text-3xl font-poppins font-bold text-customAccent mt-8">Settings</p>
             <Navbar />
             <NavbarDropDown />
         </div>
-        <div className="w-full h-[calc(100vh - 8rem)]">{children}</div>
+        <div className="w-full h-[calc(100vh - 10rem)]">{children}</div>
     </div>
   );
 }
