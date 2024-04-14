2e039b06352cfb604bde2a5619dc796bc9f73ac3
Henry <hsuarez000@citymail.cuny.edu>
2024-04-14 17:26:27 -0400

    added user goals based on weight and goal

--- topics-project/app/(welcome)/(auth)/signup/actions.js
+++ topics-project/app/(welcome)/(auth)/signup/actions.js
@@ -5,6 +5,27 @@ import { createClient } from "../../../../src/utils/supabase/server";
 
 const insertDataInUserTable = async (id, formData) => {
   try {
+    let dailyCalorieGoal, dailyFatGoal, dailyProteinGoal, dailyCarbGoal;
+    
+    // using some standard multipliers we can estimate a goal for daily goals
+    if (formData.goal === "maintain") {
+      dailyCalorieGoal = Math.round(formData.weight * 15);
+      dailyProteinGoal = Math.round(formData.weight * 0.8);
+      dailyFatGoal = Math.round(formData.weight * 0.35); 
+      dailyCarbGoal = Math.round(formData.weight * 1);
+
+    } else if (formData.goal =="gaining weight") {
+        dailyCalorieGoal = Math.round(formData.weight * 180);
+        dailyProteinGoal = Math.round(formData.weight * 1);
+        dailyFatGoal = Math.round(formData.weight * 0.38); 
+        dailyCarbGoal = Math.round(formData.weight * 1.2); 
+    }
+    else{
+      dailyCalorieGoal = Math.round(formData.weight * 140); 
+      dailyProteinGoal = Math.round(formData.weight * 0.8); 
+      dailyFatGoal = Math.round(formData.weight * 0.30); 
+      dailyCarbGoal = Math.round(formData.weight * .8);
+    }
     const supabase = createClient();
 
     const { data, error } = await supabase.from("users").insert({
@@ -12,6 +33,10 @@ const insertDataInUserTable = async (id, formData) => {
       height: formData.height,
       weight: formData.weight,
       goal: formData.goal,
+      daily_fat_goal: dailyFatGoal,
+      daily_protein_goal: dailyProteinGoal,
+      daily_carb_goal: dailyCarbGoal, 
+      daily_calorie_goal: dailyCalorieGoal,
       uuid: id,
     });
     if (error) {
