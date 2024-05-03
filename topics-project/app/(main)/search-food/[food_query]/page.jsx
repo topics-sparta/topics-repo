b7f38f501ee08aa9ff591ad091edd88880990bc4
JohanDelao <johandelao10@gmail.com>
2024-05-03 14:06:04 -0400

    progress so far

--- topics-project/app/(main)/search-food/[food_query]/page.jsx
+++ topics-project/app/(main)/search-food/[food_query]/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
 import { Loader } from "lucide-react";
 import IndividualFoodItem from "../components/IndividualFoodItem";
 
@@ -12,17 +11,9 @@ function FoodResults() {
   const searchParams = useParams();
 
   const { food_query } = searchParams; // pass into function . this may have spaces , represented as-> %20
-
-  let title_food = food_query.split("%20").join(" "); // elimated the %20 and makes it a space
-  const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [food_result, setFoods] = useState();
-  const [searchValue, setSearchValue] = useState(title_food);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    router.push(`/search-food/${searchValue}`);
-  };
   const goToFoodItem = () => {
     console.log("go to food item details"); // go to food item and either fetch by id, or pass in props
   };
@@ -43,24 +34,14 @@ function FoodResults() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center-justify-center space-y-3 w-full h-screen bg-customPrimary">
+    <div className="flex items-center justify-center w-full h-full bg-customPrimary">
       {loading ? (
-        <div className="w-full h-screen flex bg-customSecondary/25 justify-center items-center">
+        <div className="w-full h-full flex bg-customPrimary justify-center items-center">
           <Loader className="w-10 h-10 animate-spin text-customSecondary" />
         </div>
       ) : (
-        <div className="flex flex-col space-y-4 items-center justify-center w-full">
-          <form className="mt-10" onSubmit={handleSubmit}>
-            <input
-              className="w-[347px]  md:w-[900px] h-[60px] text-[24px] text-[#FFF7ED] placeholder-[#FFF7ED] bg-[#D79C59] rounded-[999px] px-10 outline-none drop-shadow-2xl "
-              placeholder="Search for any foods..."
-              value={searchValue}
-              onChange={(e) => {
-                setSearchValue(e.target.value);
-              }}
-            />
-          </form>
-          <div className="md:w-[900px] pt-5">
+        <div className="flex items-center justify-center w-full h-full">
+          <div className="w-10/12 h-full">
             {food_result?.map((item) => {
               return (
                 <>
