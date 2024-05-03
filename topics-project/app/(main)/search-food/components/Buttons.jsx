c3a607acffd20ad991d2e12e918b6f7358925222
Henry <hsuarez000@citymail.cuny.edu>
2024-05-02 23:06:53 -0400

    made div into a tag to allow for routing when clicked

--- topics-project/app/(main)/search-food/components/Buttons.jsx
+++ topics-project/app/(main)/search-food/components/Buttons.jsx
@@ -2,8 +2,9 @@ import { Flame, Salad, ScanBarcode } from "lucide-react";
 
 const Buttons = () => {
   return (
+    
     <div className="grid grid-cols-11 grid-rows-1 w-full lg:w-10/12 lg:mx-auto max-h-32 mx-2 font-medium">
-      <div className="group col-span-3 row-span-1 bg-customSecondary/20 transition-colors hover:bg-customSecondary/25 rounded-lg py-4">
+      <a href="/customfoods"className="group col-span-3 row-span-1 bg-customSecondary/20 transition-colors hover:bg-customSecondary/25 rounded-lg py-4">
         <div className="flex flex-col gap-2 text-center cursor-pointer items-center justify-center w-full h-full">
           <div className="w-12 h-12 bg-customSecondary/90 group-hover:bg-customSecondary flex justify-center items-center rounded-full transition-colors">
             <Flame className="w-6 h-6 text-customPrimary/90 group-hover:text-customPrimary transition-colors" />
@@ -12,7 +13,8 @@ const Buttons = () => {
             Add Custom Food
           </h3>
         </div>
-      </div>
+      </a>
+      
 
       <div className="col-span-1"></div>
 
