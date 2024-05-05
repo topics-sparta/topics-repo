7e499107e3ecc897e50daf04ede67cc9d9cc8f33
Henry <hsuarez000@citymail.cuny.edu>
2024-05-05 17:34:46 -0400

    part 2c

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -6,7 +6,7 @@ resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "flaskbb-environment"
   cname_prefix        = "henrysua12flaskbb"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Python 3.11"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
