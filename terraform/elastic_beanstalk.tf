6c4cbc3b83f251629afde83150ad1ede6c497260
Henry <hsuarez000@citymail.cuny.edu>
2024-05-05 17:47:00 -0400

    name update

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -1,9 +1,9 @@
 resource "aws_elastic_beanstalk_application" "application" {
-  name = "flaskbb"
+  name = "sparta"
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "flaskbb-environment"
+  name                = "sparta-environment"
   cname_prefix        = "henrysua12flaskbb"
   application         = aws_elastic_beanstalk_application.application.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
