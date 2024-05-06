6457ab1f12662b17f1729d71d8c059897f51c20a
Henry <hsuarez000@citymail.cuny.edu>
2024-05-06 18:17:32 -0400

    fixing nameing

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-zip -r "sparta_deploy.zip" ./*
+zip -r "sparta_build.zip" ./*
 
-aws s3 cp "sparta_deploy.zip" s3://terraform-state-sparta-henrysua12 
+aws s3 cp "sparta_build.zip" s3://terraform-state-sparta-henrysua12 
 
-aws elasticbeanstalk create-application-version --application-name sparta --source-bundle S3Bucket="terraform-state-sparta-henrysua12 ",S3Key="sparta_deploy.zip" --version-label "ver-$1" --description "file permissions" --region "us-west-1"
+aws elasticbeanstalk create-application-version --application-name sparta --source-bundle S3Bucket="terraform-state-sparta-henrysua12 ",S3Key="sparta_build.zip" --version-label "ver-$1" --description "file permissions" --region "us-west-1"
 
 aws elasticbeanstalk update-environment --environment-name sparta-environment --version-label "ver-$1" --region "us-west-1"
